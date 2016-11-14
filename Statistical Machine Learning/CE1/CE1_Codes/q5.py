10d05322048ed49632bf395203471c1c4413d99e
sajadazami <sajjadaazami@gmail.com>
2016-11-14 19:48:13 +0330

    q5 was added

---
+++ Statistical Machine Learning/CE1/CE1_Codes/q5.py
@@ -0,0 +1,16 @@
+"""CE1_Codes, 11/4/16, Sajad Azami"""
+
+import math
+import random
+import numpy as np
+
+__author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
+
+
+def main():
+    np.random.normal(miu, sigma, )
+    pass
+
+
+if __name__ == '__main__':
+    main()
