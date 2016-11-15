34a088f474741d060371a6530f0cbcbd867245b9
SajadAzami <sajjadaazami@gmail.com>
2016-11-15 20:16:12 +0330

    q8 was added, profiles added

---
+++ Statistical Machine Learning/CE1/CE1_Codes/q8.py
@@ -0,0 +1,21 @@
+"""CE1_Codes, 11/15/16, Sajad Azami"""
+
+import numpy as np
+from matplotlib import pyplot
+import seaborn as sns
+
+__author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
+
+sns.set_style("whitegrid")
+
+
+def simulate():
+    pass
+
+
+def main():
+    simulate()
+
+
+if __name__ == '__main__':
+    main()
