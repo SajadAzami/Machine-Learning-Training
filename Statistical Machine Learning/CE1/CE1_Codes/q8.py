c4aa566de94cea06a653418925a4e45617f07053
SajadAzami <sajjadaazami@gmail.com>
2016-11-16 00:35:02 +0330

    q8 almost done

--- Statistical Machine Learning/CE1/CE1_Codes/q8.py
+++ Statistical Machine Learning/CE1/CE1_Codes/q8.py
@@ -1,8 +1,9 @@
 """CE1_Codes, 11/15/16, Sajad Azami"""
 
 import numpy as np
-from matplotlib import pyplot
 import seaborn as sns
+from matplotlib import pyplot
+from scipy import stats
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
 
@@ -10,7 +11,16 @@ sns.set_style("whitegrid")
 
 
 def simulate():
-    pass
+    x = np.linspace(stats.uniform.ppf(0), stats.uniform.ppf(1), 100)
+    pyplot.plot(x, stats.uniform.pdf(x))
+    # pyplot.show()
+    x_i = []
+    for i in range(1, 1000):
+        x_i.append(np.random.uniform(0, 1))
+    X_n = (1 / 1000) * (sum(x_i))
+    print("Xn is : ", X_n)
+    E_Xn = 0  # TODO Find Expectation of XN
+    print("E(Xn) is : ", E_Xn)
 
 
 def main():
