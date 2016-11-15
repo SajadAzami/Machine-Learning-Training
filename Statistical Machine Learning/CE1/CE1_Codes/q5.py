7053a560a6407ee71fd54dd9ad620683d9f3e662
SajadAzami <sajjadaazami@gmail.com>
2016-11-15 12:11:12 +0330

    q5 was added, has some problems

--- Statistical Machine Learning/CE1/CE1_Codes/q5.py
+++ Statistical Machine Learning/CE1/CE1_Codes/q5.py
@@ -1,15 +1,39 @@
 """CE1_Codes, 11/4/16, Sajad Azami"""
 
-import math
-import random
+import pylab
+from scipy.stats import norm
 import numpy as np
+import matplotlib.pyplot as plt
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
 
 
+# X has distribution N(0,1), let Y = exp(X)
+# In part a, we will plot the PDF of Y
+# Then, we will create vector Xi which is 10000 random x of N(0,1)
+# and Yi which is exp(Xi). Then we will plot the histogram of y
+# Compare this to PDF we plotted in part a
+def simulate():
+    # Part a
+    x = np.linspace(-10, 10, 1000)
+    y = norm.pdf(x, loc=0, scale=1)
+    pylab.plot(x, np.exp(y))
+    pylab.show()
+
+    # Part b
+    x = []  # Xi Vector
+    y = []  # Yi Vector
+    for i in range(0, 1000):
+        x.append(np.random.normal(0, 1))
+        y.append(np.exp(x))
+        print(i)
+
+    plt.hist(y)
+    plt.show()
+
+
 def main():
-    np.random.normal(miu, sigma, )
-    pass
+    simulate()
 
 
 if __name__ == '__main__':
