8d8b629debc54ac3592831f77cb225d3bc51573b
SajadAzami <sajjadaazami@gmail.com>
2016-11-16 16:29:13 +0330

    q8 was done

--- Statistical Machine Learning/CE1/CE1_Codes/q8.py
+++ Statistical Machine Learning/CE1/CE1_Codes/q8.py
@@ -7,20 +7,43 @@ from scipy import stats
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
 
-sns.set_style("whitegrid")
+sns.set_style("darkgrid")
+
+
+# Calculates the Xn = (1/n)*(sum(x_i))
+def get_xn(n):
+    x_i = []
+    for i in range(0, n):
+        x_i.append(np.random.uniform(0, 1))
+    X_n = (1 / n) * (sum(x_i))
+    return X_n
 
 
 def simulate():
+    # Plotting the PDF of Unif(0, 1)
+    pyplot.subplot(211)
     x = np.linspace(stats.uniform.ppf(0), stats.uniform.ppf(1), 100)
+    pyplot.title('PDF of Unif(0, 1)')
     pyplot.plot(x, stats.uniform.pdf(x))
-    # pyplot.show()
-    x_i = []
-    for i in range(1, 1000):
-        x_i.append(np.random.uniform(0, 1))
-    X_n = (1 / 1000) * (sum(x_i))
-    print("Xn is : ", X_n)
-    E_Xn = 0  # TODO Find Expectation of XN
+
+    print("Xn is for n=1000: ", get_xn(1000))
+    E_Xn = 0.5  # As we know, E(Xn) is equal to mu which is 0.5
     print("E(Xn) is : ", E_Xn)
+
+    n = np.linspace(1, 1000, 1000)
+    X_ns = []
+    for i in range(1, 1001):
+        X_ns.append(get_xn(i))
+    pyplot.subplot(212)
+    pyplot.title('f(n,Xn)')
+    pyplot.plot(n, X_ns, '-g')
+
+    print("Xn for n=1", get_xn(1))
+    print("Xn for n=5", get_xn(5))
+    print("Xn for n=25", get_xn(25))
+    print("Xn for n=100", get_xn(100))
+
+    pyplot.show()
 
 
 def main():
