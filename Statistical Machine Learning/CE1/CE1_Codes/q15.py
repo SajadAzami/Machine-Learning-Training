8c040796923a3d393a70b4b393fd12f2e55eebca
SajadAzami <sajjadaazami@gmail.com>
2016-11-18 02:27:28 +0330

    q16 was done

---
+++ Statistical Machine Learning/CE1/CE1_Codes/q15.py
@@ -0,0 +1,21 @@
+"""CE1_Codes, 11/18/16, Sajad Azami"""
+# TODO complete the code
+import numpy as np
+import math
+
+__author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
+
+
+def simulate():
+    error_sum = 0
+    for i in range(0, 1000):
+        X1 = np.random.normal(loc=0, scale=1)
+        X2 = np.random.normal(loc=0, scale=1)
+
+
+def main():
+    simulate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Statistical Machine Learning/CE1/CE1_Codes/q16.py
@@ -0,0 +1,24 @@
+"""CE1_Codes, 11/18/16, Sajad Azami"""
+
+import numpy as np
+
+__author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
+
+
+# The result should be approximately 1/16, but it is almost zero
+def simulate(n, p, eps):
+    count = 0
+    for i in range(0, n):
+        X = np.random.binomial(1, p, 100)
+        X_n = (1 / 100) * (sum(X))
+        if abs(X_n - p) > eps:
+            count += 1
+    print(count / n)
+
+
+def main():
+    simulate(100, 0.3, 0.2)
+    simulate(100, 0.5, 0.2)
+
+if __name__ == '__main__':
+    main()
