dda1461b66479f8dda38dcc17a1eb5c39de5a549
SajadAzami <sajjadaazami@gmail.com>
2016-12-23 20:21:54 +0330

    q6 completed

--- Statistical Machine Learning/CE2/CE2_Codes/q6.py
+++ Statistical Machine Learning/CE2/CE2_Codes/q6.py
@@ -4,9 +4,10 @@ import math
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import scipy.stats as st
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
-sns.set_style("darkgrid")
+sns.set_style("whitegrid")
 
 
 # Simulation of bootsrap sampling
@@ -30,9 +31,40 @@ def standard_error(bootstrapped_samples):
 def simulate():
     # Generating 100 N(10,1)
     Xi = []
+    theta = []
     for i in range(0, 100):
         temp = np.random.normal(loc=10, scale=1)
         Xi.append(temp)
+    se, mean = standard_error(Xi)
+    line = np.linspace(-0, 20, 10000)
+    scale_pdf = (se ** 2)
+    pdf_values = st.norm.pdf(line, mean, scale_pdf)
+    plt.subplot(211)
+    plt.title('Posterior Density')
+    plt.plot(line, pdf_values)
+
+    simulated_from_posterior = []
+    for i in range(0, 1000):
+        simulated_from_posterior.append(np.random.normal(mean, scale_pdf))
+    plt.subplot(212)
+    plt.hist(simulated_from_posterior)
+    plt.title('1000 Simulate Draws from Posterior')
+    plt.show()
+    # Part d, e, f
+    for i in range(0, 100):
+        temp = bootstrap(Xi)
+        miu = sum(temp) * 1 / len(temp)
+        theta.append(math.exp(miu))
+    plt.hist(theta)
+    plt.show()
+    theta_hat = math.exp(sum(Xi) * 1 / len(Xi))
+    # Percentile
+    percentile_CI = (np.percentile(theta, 0.025), np.percentile(theta, 97.5))
+    print('Percentile Interval:', percentile_CI)
+    # Pivotal
+    pivotal_CI = (2 * theta_hat - np.percentile(theta, 97.5)
+                  , 2 * theta_hat - np.percentile(theta, 0.025))
+    print('Pivotal Interval:', pivotal_CI)
 
 
 def main():
