09b32874e21344cd744559359d30149004295190
SajadAzami <sajjadaazami@gmail.com>
2017-01-21 17:59:02 +0330

    Linear Regression done

--- Statistical Machine Learning/Final Project/Project 1/Linear Regression/linear_regression.py
+++ Statistical Machine Learning/Final Project/Project 1/Linear Regression/linear_regression.py
@@ -3,6 +3,7 @@
 import data_preparation
 import matplotlib.pyplot as plt
 import seaborn as sns
+import math
 from matplotlib import gridspec
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
@@ -11,15 +12,22 @@ sns.set_style("darkgrid")
 
 # Fits a line that minimizes Residual Sum of Errors
 # The line is Yi = B0 + B1*Xi
-# feature_vector and response_vector are numpy arrays
-def rss_regressor(feature_vector, respones_vector):
-    Y_bar = sum(respones_vector) / len(respones_vector)
+# feature_vector(Xi) and response_vector(Yi) are numpy arrays
+# returns B0, B1, sigma2_hat(estimated variance of epsilon) and standard errors of B0 and B1
+def rss_regressor(feature_vector, response_vector):
+    Y_bar = sum(response_vector) / len(response_vector)
     X_bar = sum(feature_vector) / len(feature_vector)
-    B1_hat = sum((feature_vector - X_bar) * (respones_vector - Y_bar)) / sum((feature_vector - X_bar) ** 2)
+    B1_hat = sum((feature_vector - X_bar) * (response_vector - Y_bar)) / sum((feature_vector - X_bar) ** 2)
     B0_hat = Y_bar - B1_hat * X_bar
-    return B0_hat, B1_hat
+    sigma2_hat = 1 / (len(feature_vector) - 2) * sum((response_vector -
+                                                      (B0_hat + B1_hat * feature_vector)) ** 2)
+    S_x = (1 / len(feature_vector)) * sum((feature_vector - X_bar) ** 2)
+    standard_error_B1 = math.sqrt(sigma2_hat) / S_x * math.sqrt(len(feature_vector))
+    standard_error_B0 = standard_error_B1 * math.sqrt(sum(feature_vector ** 2) / len(feature_vector))
+    return B0_hat, B1_hat, sigma2_hat, standard_error_B0, standard_error_B1
 
 
+# Gets points of fitted line for plotting purpose
 def get_points(line, B0, B1):
     points = []
     for i in line:
--- Statistical Machine Learning/Final Project/Project 1/Linear Regression/main.py
+++ Statistical Machine Learning/Final Project/Project 1/Linear Regression/main.py
@@ -25,36 +25,49 @@ print('Test data size:', len(test_data_1))
 # Scatter plot each feature vs label
 fig = plt.figure()
 gs = gridspec.GridSpec(3, 3)
-ax1 = fig.add_subplot(gs[0, 0])
-ax1.scatter(train_data_1.get(0), train_label_1)
-ax1.title.set_text('Feature 1')
-ax2 = fig.add_subplot(gs[0, 1])
-ax2.scatter(train_data_1.get(1), train_label_1)
-ax2.title.set_text('Feature 2')
-ax3 = fig.add_subplot(gs[0, 2])
-ax3.scatter(train_data_1.get(2), train_label_1)
-ax3.title.set_text('Feature 3')
-ax4 = fig.add_subplot(gs[1, 0])
-ax4.scatter(train_data_1.get(3), train_label_1)
-ax4.title.set_text('Feature 4')
-ax5 = fig.add_subplot(gs[1, 1])
-ax5.scatter(train_data_1.get(4), train_label_1)
-ax5.title.set_text('Feature 5')
-ax6 = fig.add_subplot(gs[1, 2])
-ax6.scatter(train_data_1.get(5), train_label_1)
-ax6.title.set_text('Feature 6')
-ax7 = fig.add_subplot(gs[2, 0])
-ax7.scatter(train_data_1.get(6), train_label_1)
-ax7.title.set_text('Feature 7')
-ax8 = fig.add_subplot(gs[2, 1])
-ax8.scatter(train_data_1.get(7), train_label_1)
-ax8.title.set_text('Feature 8')
+counter = 0
+for i in range(0, 3):
+    for j in range(0, 3):
+        counter += 1
+        if counter == 9:
+            break
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.scatter(train_data_1.get(counter - 1), train_label_1)
+        ax_temp.title.set_text(('Feature ' + str(counter)))
 plt.show()
 
 # Finding Simple Linear Regression models for each feature with RSS metric
-linear_regression_1 = linear_regression.rss_regressor(train_data_1.get(0).values, train_label_1)
-print(linear_regression_1)
-line = np.linspace(-5, 5, 10000)
-plt.scatter(train_data_1.get(0), train_label_1)
-plt.plot(line, linear_regression.get_points(line, linear_regression_1[0], linear_regression_1[1]))
+linear_regressions = []
+for i in range(0, 8):
+    linear_regressions.append(linear_regression.rss_regressor(train_data_1.get(i).values, train_label_1))
+
+# Plotting Lines fitted with each feature
+fig = plt.figure()
+gs = gridspec.GridSpec(3, 3)
+counter = 0
+for i in range(0, 3):
+    for j in range(0, 3):
+        counter += 1
+        if counter == 9:
+            break
+        line = np.linspace(min(train_data_1.get(counter - 1)) - 3,
+                           max(train_data_1.get(counter - 1) + 3), 10000)
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.scatter(train_data_1.get(counter - 1), train_label_1)
+        ax_temp.plot(line, linear_regression.get_points(line, linear_regressions[i][0],
+                                                        linear_regressions[i][1]))
+        ax_temp.title.set_text(('Line with Feature ' + str(counter)))
 plt.show()
+
+# Reporting Linear Regression Characteristics
+for i in range(0, len(linear_regressions)):
+    regression_temp = linear_regressions[i]
+    b0_hat = regression_temp[0]
+    b1_hat = regression_temp[1]
+    estimated_epsilon = regression_temp[2]
+    standard_error_b0 = regression_temp[3]
+    standard_error_b1 = regression_temp[4]
+    print('Simple Linear Regression with Feature' + str(i + 1) +
+          '\nEstimated (Beta0, Beta1): (' + str(b0_hat) + ', ' + str(b1_hat) + ')\n' +
+          'Standard Error of Beta0 and Beta1: (' + str(standard_error_b0) + ', ' + str(standard_error_b1) +
+          ')\nEstimated Variance of Epsilon: ' + str(estimated_epsilon) + '\n')
