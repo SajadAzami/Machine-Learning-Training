01cc12a49580193710b5aabe54ec01e51a0b45a1
SajadAzami <sajjadaazami@gmail.com>
2017-02-01 03:21:03 +0330

    project one finished

--- Statistical Machine Learning/Final Project/Project 1/Linear Regression/part_one/main.py
+++ Statistical Machine Learning/Final Project/Project 1/Linear Regression/part_one/main.py
@@ -6,6 +6,7 @@ import numpy as np
 import seaborn as sns
 from matplotlib import gridspec
 import matplotlib.pyplot as plt
+import random
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
 sns.set_style("darkgrid")
@@ -22,65 +23,65 @@ test_label_1 = label_1[400:]
 print('Train data size:', len(train_data_1))
 print('Test data size:', len(test_data_1))
 
-# # Scatter plot each feature vs label
-# fig = plt.figure()
-# gs = gridspec.GridSpec(3, 3)
-# counter = 0
-# for i in range(0, 3):
-#     for j in range(0, 3):
-#         counter += 1
-#         if counter == 9:
-#             break
-#         ax_temp = fig.add_subplot(gs[i, j])
-#         ax_temp.scatter(train_data_1.get(counter - 1), train_label_1)
-#         ax_temp.title.set_text(('Feature ' + str(counter)))
-# plt.show()
-#
-# # Finding Simple Linear Regression models for each feature with RSS metric
-# linear_regressions = []
-# for i in range(0, 8):
-#     linear_regressions.append(
-#         linear_regression.rss_regressor(train_data_1.get(i).values, train_label_1,
-#                                         test_data_1.get(i), test_label_1))
-#
-# # Plotting Lines fitted with each feature
-# fig = plt.figure()
-# gs = gridspec.GridSpec(3, 3)
-# counter = 0
-# for i in range(0, 3):
-#     for j in range(0, 3):
-#         counter += 1
-#         if counter == 9:
-#             break
-#         line = np.linspace(min(train_data_1.get(counter - 1)) - 3,
-#                            max(train_data_1.get(counter - 1) + 3), 10000)
-#         ax_temp = fig.add_subplot(gs[i, j])
-#         ax_temp.scatter(train_data_1.get(counter - 1), train_label_1)
-#         ax_temp.plot(line, linear_regression.get_points(line, linear_regressions[counter - 1][0],
-#                                                         linear_regressions[counter - 1][1]))
-#         ax_temp.title.set_text(('Line with Feature ' + str(counter)))
-# plt.show()
-#
-# # Reporting Linear Regression Characteristics for train and test Data
-# for i in range(0, len(linear_regressions)):
-#     regression_temp = linear_regressions[i]
-#     b0_hat = regression_temp[0]
-#     b1_hat = regression_temp[1]
-#     estimated_epsilon = regression_temp[2]
-#     standard_error_b0 = regression_temp[3]
-#     standard_error_b1 = regression_temp[4]
-#     RSS_train = regression_temp[5]
-#     R2_train = regression_temp[6]
-#     RSS_test = regression_temp[7]
-#     R2_test = regression_temp[8]
-#     print('Simple Linear Regression with Feature' + str(i + 1) +
-#           '\nEstimated (Beta0, Beta1): (' + str(b0_hat) + ', ' + str(b1_hat) + ')\n' +
-#           'Standard Error of Beta0 and Beta1: (' + str(standard_error_b0) + ', ' + str(standard_error_b1) +
-#           ')\nEstimated Variance of Epsilon: ' + str(estimated_epsilon) + '\n' +
-#           'RSS_train: ' + str(RSS_train) + str('\n') +
-#           'R2_train: ' + str(R2_train) + str('\n') +
-#           'RSS_test: ' + str(RSS_test) + str('\n') +
-#           'R2_test: ' + str(R2_test) + str('\n'))
+# Scatter plot each feature vs label
+fig = plt.figure()
+gs = gridspec.GridSpec(3, 3)
+counter = 0
+for i in range(0, 3):
+    for j in range(0, 3):
+        counter += 1
+        if counter == 9:
+            break
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.scatter(train_data_1.get(counter - 1), train_label_1)
+        ax_temp.title.set_text(('Feature ' + str(counter)))
+plt.show()
+
+# Finding Simple Linear Regression models for each feature with RSS metric
+linear_regressions = []
+for i in range(0, 8):
+    linear_regressions.append(
+        linear_regression.rss_regressor(train_data_1.get(i).values, train_label_1,
+                                        test_data_1.get(i), test_label_1))
+
+# Plotting Lines fitted with each feature
+fig = plt.figure()
+gs = gridspec.GridSpec(3, 3)
+counter = 0
+for i in range(0, 3):
+    for j in range(0, 3):
+        counter += 1
+        if counter == 9:
+            break
+        line = np.linspace(min(train_data_1.get(counter - 1)) - 3,
+                           max(train_data_1.get(counter - 1) + 3), 10000)
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.scatter(train_data_1.get(counter - 1), train_label_1)
+        ax_temp.plot(line, linear_regression.get_points(line, linear_regressions[counter - 1][0],
+                                                        linear_regressions[counter - 1][1]))
+        ax_temp.title.set_text(('Line with Feature ' + str(counter)))
+plt.show()
+
+# Reporting Linear Regression Characteristics for train and test Data
+for i in range(0, len(linear_regressions)):
+    regression_temp = linear_regressions[i]
+    b0_hat = regression_temp[0]
+    b1_hat = regression_temp[1]
+    estimated_epsilon = regression_temp[2]
+    standard_error_b0 = regression_temp[3]
+    standard_error_b1 = regression_temp[4]
+    RSS_train = regression_temp[5]
+    R2_train = regression_temp[6]
+    RSS_test = regression_temp[7]
+    R2_test = regression_temp[8]
+    print('Simple Linear Regression with Feature' + str(i + 1) +
+          '\nEstimated (Beta0, Beta1): (' + str(b0_hat) + ', ' + str(b1_hat) + ')\n' +
+          'Standard Error of Beta0 and Beta1: (' + str(standard_error_b0) + ', ' + str(standard_error_b1) +
+          ')\nEstimated Variance of Epsilon: ' + str(estimated_epsilon) + '\n' +
+          'RSS_train: ' + str(RSS_train) + str('\n') +
+          'R2_train: ' + str(R2_train) + str('\n') +
+          'RSS_test: ' + str(RSS_test) + str('\n') +
+          'R2_test: ' + str(R2_test) + str('\n'))
 
 # Starting with Feature4(as the best feature) and adding features, then checking AIC, RSS and R2
 current_AIC = linear_regression.get_log_likelihood(train_label_1, train_data_1.get(3)) - 1
@@ -145,3 +146,15 @@ plt.title('RSS vs Number of Used Features')
 plt.show()
 
 # Testing Full-Feature model with different number of train data
+features_count = [50, 100, 250, 300, 350, 400]
+RSS = []
+for i in features_count:
+    indices = random.sample(range(0, 400), i)
+    temp_data = features_to_use.T[indices].reshape(i, 8)
+    temp_label = train_label_1[indices]
+    RSS.append(linear_regression.multivariate_rss_regressor(
+        temp_data, temp_label, None, None)[1])
+plt.plot(features_count, RSS)
+plt.title('Train RSS vs Number of Train')
+plt.show()
+print(RSS)
