46a0be60b31e71a87999d6380926869ee18b2527
SajadAzami <sajjadaazami@gmail.com>
2017-02-05 00:33:06 +0330

    scatter plots added

--- Statistical Machine Learning/Final Project/Project 3/PGM/main.py
+++ Statistical Machine Learning/Final Project/Project 3/PGM/main.py
@@ -17,10 +17,6 @@ hungarian = data_preparation.read_data('./data_set/processed.hungarian.data.txt'
 switzerland = data_preparation.read_data('./data_set/processed.switzerland.data.txt')
 va = data_preparation.read_data('./data_set/processed.va.data.txt')
 print('Data set Loaded!')
-print(cleveland.shape)
-print(hungarian.shape)
-print(switzerland.shape)
-print(va.shape)
 
 # Merge datasets
 frames = [cleveland, hungarian, switzerland, va]
@@ -29,8 +25,6 @@ all_city_data = pd.concat(frames)
 # Split label and features
 all_city_data, all_city_label = data_preparation.split_label(all_city_data, 13)
 all_city_label = all_city_label.reshape(len(all_city_label), 1)
-print(all_city_label.shape)
-print(all_city_data.shape)
 
 # Filling missing values with each columns mean for column [0, 3, 4, 7, 9] and mode for the rest
 all_city_data = all_city_data.replace('?', -10)
@@ -45,9 +39,39 @@ for i in mean_indices:
 for i in mode_indices:
     all_city_data[i] = all_city_data[i].fillna(all_city_data[i].mode()[0])
 
-print(all_city_data)
-
 # Decreasing label classes from 5 to 2(0 or 1)
 for i in range(0, len(all_city_label)):
     if all_city_label[i] != 0:
         all_city_label[i] = 1
+
+print(all_city_label.shape)
+print(all_city_data.shape)
+# Scatter plot each feature vs label after filling missing values
+fig = plt.figure()
+gs = gridspec.GridSpec(3, 3)
+counter = 0
+# Discrete values
+for i in range(0, 3):
+    for j in range(0, 3):
+        if counter == 8:
+            break
+        # print(counter)
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.scatter(all_city_data.get(mode_indices[counter]), all_city_label)
+        ax_temp.title.set_text(('Feature ' + str(mode_indices[counter])))
+        counter += 1
+plt.show()
+# Continuous values
+fig = plt.figure()
+gs = gridspec.GridSpec(2, 3)
+counter = 0
+for i in range(0, 2):
+    for j in range(0, 3):
+        if counter == 5:
+            break
+        # print(counter)
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.scatter(all_city_data.get(mean_indices[counter]), all_city_label)
+        ax_temp.title.set_text(('Feature ' + str(mean_indices[counter])))
+        counter += 1
+plt.show()
