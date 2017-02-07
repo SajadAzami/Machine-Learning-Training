6a61bb578ad787c64f9baa1db7d83f73f08f494b
SajadAzami <sajjadaazami@gmail.com>
2017-02-08 03:24:28 +0330

    naive bayes done, LOOCV fixed

--- Statistical Machine Learning/Final Project/Project 3/PGM/main.py
+++ Statistical Machine Learning/Final Project/Project 3/PGM/main.py
@@ -7,9 +7,25 @@ import matplotlib.pyplot as plt
 import random
 import pandas as pd
 import data_preparation
+from sklearn.naive_bayes import GaussianNB
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
 sns.set_style("darkgrid")
+
+
+# Calculates Leave One Out Cross Validation Error
+def get_LOOCV(features_to_use, train_label):
+    RSS_list = []
+    for i in range(0, features_to_use.shape[1]):
+        temp_data = features_to_use.drop(i, axis=0)
+        temp_label = np.delete(train_label.reshape(len(train_label), 1), i, 0)
+        gnb = GaussianNB()
+        gnb.fit(temp_data, temp_label)
+        pred = gnb.predict(features_to_use.iloc[[i]])
+        RSS_list.append(sum((train_label[i] - pred) ** 2))
+    LOOCV = sum(RSS_list) / len(RSS_list)
+    return LOOCV
+
 
 # Load dataset
 cleveland = data_preparation.read_data('./data_set/processed.cleveland.data.txt')
@@ -25,6 +41,7 @@ all_city_data = pd.concat(frames)
 # Split label and features
 all_city_data, all_city_label = data_preparation.split_label(all_city_data, 13)
 all_city_label = all_city_label.reshape(len(all_city_label), 1)
+all_city_data = all_city_data.reset_index(drop=True)
 
 # Filling missing values with each columns mean for column [0, 3, 4, 7, 9] and mode for the rest
 all_city_data = all_city_data.replace('?', -10)
@@ -56,32 +73,44 @@ all_city_data[7] = pd.cut(all_city_data[7].values, 5,
 all_city_data[9] = pd.cut(all_city_data[9].values, 3,
                           labels=[0, 1, 2]).astype(list)
 
-# Scatter plot each feature vs label after filling missing values
-fig = plt.figure()
-gs = gridspec.GridSpec(3, 3)
-counter = 0
-# Discrete values
-for i in range(0, 3):
-    for j in range(0, 3):
-        if counter == 8:
-            break
-        # print(counter)
-        ax_temp = fig.add_subplot(gs[i, j])
-        ax_temp.scatter(all_city_data.get(mode_indices[counter]), all_city_label)
-        ax_temp.title.set_text(('Feature ' + str(mode_indices[counter])))
-        counter += 1
-plt.show()
-# Continuous values
-fig = plt.figure()
-gs = gridspec.GridSpec(2, 3)
-counter = 0
-for i in range(0, 2):
-    for j in range(0, 3):
-        if counter == 5:
-            break
-        # print(counter)
-        ax_temp = fig.add_subplot(gs[i, j])
-        ax_temp.scatter(all_city_data.get(mean_indices[counter]), all_city_label)
-        ax_temp.title.set_text(('Feature ' + str(mean_indices[counter])))
-        counter += 1
-plt.show()
+# # Scatter plot each feature vs label after filling missing values
+# fig = plt.figure()
+# gs = gridspec.GridSpec(3, 3)
+# counter = 0
+# # Discrete values
+# for i in range(0, 3):
+#     for j in range(0, 3):
+#         if counter == 8:
+#             break
+#         # print(counter)
+#         ax_temp = fig.add_subplot(gs[i, j])
+#         ax_temp.scatter(all_city_data.get(mode_indices[counter]), all_city_label)
+#         ax_temp.title.set_text(('Feature ' + str(mode_indices[counter])))
+#         counter += 1
+# plt.show()
+# # Continuous values
+# fig = plt.figure()
+# gs = gridspec.GridSpec(2, 3)
+# counter = 0
+# for i in range(0, 2):
+#     for j in range(0, 3):
+#         if counter == 5:
+#             break
+#         # print(counter)
+#         ax_temp = fig.add_subplot(gs[i, j])
+#         ax_temp.scatter(all_city_data.get(mean_indices[counter]), all_city_label)
+#         ax_temp.title.set_text(('Feature ' + str(mean_indices[counter])))
+#         counter += 1
+# plt.show()
+
+
+# Learning naive bayes model from data
+all_city_label = all_city_label.reshape(len(all_city_label), )
+gnb = GaussianNB()
+gnb.fit(all_city_data, all_city_label)
+pred = gnb.predict(all_city_data)
+print("Number of mislabeled points out of a total " + str(all_city_data.shape[0]) + ' points: ' + (
+    str((all_city_label - pred).sum())))
+
+# LOOCV risk
+print(get_LOOCV(all_city_data, all_city_label))
