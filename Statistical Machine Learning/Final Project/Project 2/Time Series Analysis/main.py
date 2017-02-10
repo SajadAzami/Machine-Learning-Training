9c65bfa296a7aed9ce9663baa32a272b10a60425
SajadAzami <sajjadaazami@gmail.com>
2017-02-10 22:12:00 +0330

    pre final report commit

--- Statistical Machine Learning/Final Project/Project 2/Time Series Analysis/main.py
+++ Statistical Machine Learning/Final Project/Project 2/Time Series Analysis/main.py
@@ -1,20 +1,15 @@
 """Time Series Analysis, 2/8/17, Sajad Azami"""
 
-import numpy as np
-import seaborn as sns
-from matplotlib import gridspec
 import matplotlib.pyplot as plt
-import random
+import numpy as np
 import pandas as pd
-import data_preparation
-from arch import arch_model
-import statsmodels.formula.api as smf
-import statsmodels.tsa.api as smt
-import statsmodels.api as sm
-import scipy.stats as scs
-from pandas.tools.plotting import lag_plot
+import seaborn as sns
 from pandas.tools.plotting import autocorrelation_plot
+from pandas.tools.plotting import lag_plot
 from sklearn.metrics import mean_absolute_error
+from statsmodels.tsa.ar_model import AR
+
+import data_preparation
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
 sns.set_style("dark")
@@ -26,24 +21,24 @@ print('Data set Loaded!')
 print(data.shape)
 print(label.shape)
 
-# # Plot 2 weeks of data points
-# line = np.linspace(0, 336, 336)
-# plt.plot(line, label[0:336])
-# plt.xlabel('Hour')
-# plt.ylabel('Power Demand')
-# plt.title('Power Demand of first 14 days')
-# plt.show()
-# lag_plot(label)
-#
-# # Plotting the lag plot of target feature
-# plt.title('Lag plot of Power Demand')
-# plt.xlabel('P(t)')
-# plt.ylabel('P(t+1)')
-# plt.show()
-#
-# # Plotting auto-correlation
-# autocorrelation_plot(label[0:1000])
-# plt.show()
+# Plot 2 weeks of data points
+line = np.linspace(0, 336, 336)
+plt.plot(line, label[0:336])
+plt.xlabel('Hour')
+plt.ylabel('Power Demand')
+plt.title('Power Demand of first 14 days')
+plt.show()
+lag_plot(label)
+
+# Plotting the lag plot of target feature
+plt.title('Lag plot of Power Demand')
+plt.xlabel('P(t)')
+plt.ylabel('P(t+1)')
+plt.show()
+
+# Plotting auto-correlation
+autocorrelation_plot(label[0:1000])
+plt.show()
 
 # Splitting train and test data
 train_data, test_data = data[0:119832], data[119832:]
@@ -58,12 +53,12 @@ train_X, train_y = train[:, 0], train[:, 1]
 test_X, test_y = test[:, 0], test[:, 1]
 
 
-# persistence model
+# Persistence model
 def model_persistence(x):
     return x
 
 
-# walk-forward validation
+# Walk-forward validation
 predictions = list()
 for x in test_X:
     y_hat = model_persistence(x)
@@ -71,6 +66,30 @@ for x in test_X:
 test_score = mean_absolute_error(test_y, predictions)
 print('Test MAE: %.3f' % test_score)
 # plot predictions vs expected
-plt.plot(test_y)
-plt.plot(predictions, color='red')
+plt.plot(test_y, label="real values")
+plt.plot(predictions, color='red', label="predictions")
+plt.legend(loc='upper left')
+plt.show()
+
+
+# Implementing Auto Regression Model
+# Training Autoregression
+print(train[:, 1])
+model = AR(train_y)
+model_fit = model.fit()
+print('Lag: %s' % model_fit.k_ar)
+print('Coefficients: %s' % model_fit.params)
+
+# Making predictions
+predictions = model_fit.predict(start=len(train_y), end=len(train_y) + len(test_y) - 1, dynamic=False)
+# for i in range(len(predictions)):
+#     print('predicted=%f, expected=%f' % (predictions[i], test[i]))
+error = mean_absolute_error(test_y, predictions)
+print('Test MAE: %.3f' % error)
+
+# Plotting results
+plt.plot(test_y, label="real values")
+plt.plot(predictions, color='red', label="predictions")
+plt.legend(loc='upper left')
+
 plt.show()
